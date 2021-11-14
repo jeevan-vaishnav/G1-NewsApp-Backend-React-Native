a3b1fcf75e87327df74ab2a678a0b44b0fa5a9e4
Jeevan0109 <jeevanvaishnavv@gmail.com>
2021-11-14 08:22:14 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const newsRouter = require("./routers/news");
+
+// added later to find out incoming and outgoing requests.
+const morgan = require("morgan");
+app.use(morgan("dev"));
+
+app.use(express.static("public"));
+app.use(express.static("data/uploads"));
+app.use("/api", newsRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}.`);
+});
